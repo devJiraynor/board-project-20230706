1cdc16bb5459f697ad0def5ce88bb26246c18d11
devjiraynor <jiraynor@naver.com>
2023-09-21 12:52:19 +0900

    2023.09.21. 12:50

---
+++ board-back/src/main/java/com/seojihoon/boardback/service/AuthService.java
@@ -0,0 +1,5 @@
+package com.seojihoon.boardback.service;
+
+public interface AuthService {
+    
+}
---
+++ board-back/src/main/java/com/seojihoon/boardback/service/implement/AuthServiceImplement.java
@@ -0,0 +1,10 @@
+package com.seojihoon.boardback.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.seojihoon.boardback.service.AuthService;
+
+@Service
+public class AuthServiceImplement implements AuthService {
+    
+}
