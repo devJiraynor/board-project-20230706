1cf2c6f9a8da9f4d81b9c9ddfa3c7cf10bf8f1ea
devjiraynor <jiraynor@naver.com>
2023-09-22 12:53:44 +0900

    2023.09.22. 12:50

--- board-back/src/main/java/com/seojihoon/boardback/service/implement/AuthServiceImplement.java
+++ board-back/src/main/java/com/seojihoon/boardback/service/implement/AuthServiceImplement.java
@@ -2,27 +2,59 @@ package com.seojihoon.boardback.service.implement;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.seojihoon.boardback.dto.request.auth.SignUpRequestDto;
 import com.seojihoon.boardback.dto.response.ResponseDto;
 import com.seojihoon.boardback.dto.response.auth.SignUpResponseDto;
+import com.seojihoon.boardback.entity.UserEntity;
+import com.seojihoon.boardback.repository.UserRepository;
 import com.seojihoon.boardback.service.AuthService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImplement implements AuthService {
+
+    private final UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
     public ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto) {
-        
+
         try {
+
+            String email = dto.getEmail();
+            String nickname = dto.getNickname();
+            String telNumber = dto.getTelNumber();
+
+            boolean hasEmail = userRepository.existsByEmail(email);
+            if (hasEmail) return SignUpResponseDto.duplicateEmail();
+
+            boolean hasNickname = userRepository.existsByNickname(nickname);
+            if (hasNickname) return SignUpResponseDto.duplicateNickname();
+
+            boolean hasTelNumber = userRepository.existsByTelNumber(telNumber);
+            if (hasTelNumber) return SignUpResponseDto.duplicateTelNumber();
+
+            String password = dto.getPassword();
+            String encodedPassword = passwordEncoder.encode(password);
+
+            dto.setPassword(encodedPassword);
+
+            UserEntity userEntity = new UserEntity(dto);
+            userRepository.save(userEntity);
 
         } catch(Exception exception) {
             exception.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto("DBE", "Database Error."));
+            return ResponseDto.databaseError();
         }
 
-        return null;
+        return SignUpResponseDto.success();
 
     }
     
