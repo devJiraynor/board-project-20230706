f6a5d6f3b4eb889542256539eb1f11488aac8937
devjiraynor <jiraynor@naver.com>
2023-09-25 12:51:09 +0900

    2023.09.25. 12:50

---
+++ board-back/src/main/java/com/seojihoon/boardback/service/implement/UserServiceImplement.java
@@ -0,0 +1,13 @@
+package com.seojihoon.boardback.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.seojihoon.boardback.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImplement implements UserService {
+    
+}
