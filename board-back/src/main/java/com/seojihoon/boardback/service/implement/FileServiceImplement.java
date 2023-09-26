2c40e7a55645743d1ca786424f08796cca3ce33c
devjiraynor <jiraynor@naver.com>
2023-09-26 10:51:12 +0900

    2023.09.26. 10:50

---
+++ board-back/src/main/java/com/seojihoon/boardback/service/implement/FileServiceImplement.java
@@ -0,0 +1,16 @@
+package com.seojihoon.boardback.service.implement;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.seojihoon.boardback.service.FileService;
+
+@Service
+public class FileServiceImplement implements FileService {
+
+    @Override
+    public String upload(MultipartFile file) {
+        
+    }
+    
+}
