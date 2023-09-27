87ef3cdd1d1f6249f5e1834bbb82bb20ac729d3e
devjiraynor <jiraynor@naver.com>
2023-09-27 11:52:05 +0900

    2023.09.27. 11:50

--- board-back/src/main/java/com/seojihoon/boardback/repository/UserRepository.java
+++ board-back/src/main/java/com/seojihoon/boardback/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.seojihoon.boardback.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.seojihoon.boardback.entity.UserEntity;
