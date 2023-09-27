c9d9a3bb24d8dba9688986a5219b82270a3f5978
devjiraynor <jiraynor@naver.com>
2023-09-27 10:42:42 +0900

    2023.09.27. 10:50

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -3,6 +3,8 @@ import { SignInRequestDto, SignUpRequestDto } from './dto/request/auth';
 import { SignInResponseDto, SignUpResponseDto } from './dto/response/auth';
 import ResponseDto from './dto/response';
 import { GetSignInUserResponseDto, GetUserResponseDto } from './dto/response/user';
+import { PostBoardRequestDto } from './dto/request/board';
+import { PostBoardResponseDto } from './dto/response/board';
 
 // description: Domain URL //
 const DOMAIN = 'http://localhost:4000';
@@ -48,6 +50,25 @@ export const signInRequest = async (requestBody: SignInRequestDto) => {
         });
     return result;
 };
+
+// description: post board API end point //
+const POST_BOARD_URL = () => `${API_DOMAIN}/board`;
+
+// description: post board request //
+export const postBoardRequest = async (requestBody: PostBoardRequestDto, token: string) => {
+    const result = await axios.post(POST_BOARD_URL(), requestBody, authorization(token))
+        .then(response => {
+            const responseBody: PostBoardResponseDto = response.data;
+            const { code } = responseBody;
+            return code;
+        })
+        .catch(error => {
+            const responseBody: ResponseDto = error.response.data;
+            const { code } = responseBody;
+            return code;
+        });
+    return result;
+}
 
 // description: get sign in user API end point //
 const GET_SIGN_IN_USER_URL = () => `${API_DOMAIN}/user`;
