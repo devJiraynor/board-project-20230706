9db0d9db2746878b38601c5cb025258f076128ba
devjiraynor <jiraynor@naver.com>
2023-10-05 13:43:45 +0900

    2023.10.05. 13:50

--- board-back/src/main/java/com/seojihoon/boardback/service/BoardService.java
+++ board-back/src/main/java/com/seojihoon/boardback/service/BoardService.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 
 import com.seojihoon.boardback.dto.request.board.PostBoardRequestDto;
 import com.seojihoon.boardback.dto.response.board.GetBoardResponseDto;
+import com.seojihoon.boardback.dto.response.board.GetCommentListResponseDto;
 import com.seojihoon.boardback.dto.response.board.GetFavoriteListResponseDto;
 import com.seojihoon.boardback.dto.response.board.GetLatestBoardListResponseDto;
 import com.seojihoon.boardback.dto.response.board.PostBoardResponseDto;
@@ -15,6 +16,9 @@ public interface BoardService {
 
     ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber);
     ResponseEntity<? super GetFavoriteListResponseDto> getFavorietList(Integer boardNumber);
+    
+    ResponseEntity<? super GetCommentListResponseDto> getCommentList(Integer boardNumber);
+    
     ResponseEntity<? super GetLatestBoardListResponseDto> getLatestBoardList();
 
     ResponseEntity<? super PutFavoriteResponseDto> putFavorite(Integer boardNumber, String email);
